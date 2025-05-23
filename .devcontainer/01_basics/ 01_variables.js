1d173fbdf5d881ac4ee37fb9c9e5666e5a4c63d7
utkarsha34 <utkarshachauhan79@gmail.com>
2025-05-23 20:37:44 +0000

    cont config done

---
+++ .devcontainer/01_basics/ 01_variables.js
@@ -0,0 +1,17 @@
+const account_id = 144553
+let acount_email = "era@gmail.com"
+var account_password = "12345"
+account_city = "bareilly"
+
+// account_id = 2 // not allowed
+ 
+account_email = "utkarsha@gmail.com"
+account_password = "6789"
+account_city = " bengaluru"
+
+
+console.log(account_id);
+
+console.table([account_id, account_email, account_password, account_city])
+
+
