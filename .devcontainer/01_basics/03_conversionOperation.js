6c4b292bbc88925a130bd38edf8934114a81ec58
utkarsha34 <utkarshachauhan79@gmail.com>
2025-05-26 08:40:36 +0000

    conversion of data types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = " utkarsha "     // its not showing 0 , when writting "null" (boolean)//
+
+//console.log( typeof score);
+//console.log( typeof (score))
+
+console.log( score)
+
+//let valueInnumber = Number(score)
+//console.log( typeof valueInnumber);
+//console.log( valueInnumber)
+
+
+// "33" => 33
+// "33abc" => nan
+// true => 1; false => 0
+
+//let isLoggedIn = " era "
+
+//let booleanIsLoggedIn = Boolean( isLoggedIn)
+//console.log( booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "era" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);     //not working//
